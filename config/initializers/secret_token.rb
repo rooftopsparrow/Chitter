d3efa0c401617a5ff4b252b1b4a55a09091e51fe
Jonathan Nicholson <jonathandavidnicholson@gmail.com>
2011-06-30 11:52:16 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chitter::Application.config.secret_token = '473c7c0c02b7b63ca59b6035a5c72b1cec5b6fea57730ec1580107dce7510af208c768c65b080c8a443314533dc81fd5544c38cf248387413d6b0d6c7f2fd735'
