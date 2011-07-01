47b092e0dedfc8f9b83353935424a714611b1692
Jonathan Nicholson <jonathandavidnicholson@gmail.com>
2011-07-01 01:45:55 -0600

    Adding a help page and CSS

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
+  before(:each) do
+    @base_title = "Chitter | "
+  end
+  
   describe "GET 'home'" do
     
     it "should be successful" do
@@ -12,7 +16,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Chitter | Home")
+      response.should have_selector("title", :content => @base_title + "Home")
     end
 
   end
@@ -26,7 +30,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Chitter | Contact")
+      response.should have_selector("title", :content => @base_title + "Contact")
     end
     
   end
@@ -40,9 +44,23 @@ describe PagesController do
     
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Chitter | About")
+      response.should have_selector("title", :content => @base_title + "About")
     end
     
   end
-
+  
+  describe "GET 'help'" do
+    
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title = "Help")
+    end
+  
+  end
+    
 end
