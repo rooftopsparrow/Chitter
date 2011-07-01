e6787cd037d5bb3eb198901a6fc7961bb906502b
Jonathan Nicholson <jonathandavidnicholson@gmail.com>
2011-07-01 00:27:24 -0600

    Completed static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,48 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
+    
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-  end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Chitter | Home")
+    end
 
+  end
+  
   describe "GET 'contact'" do
+    
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Chitter | Contact")
+    end
+    
+  end
+  
+  describe "GET 'about'" do
+    
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Chitter | About")
+    end
+    
   end
 
 end
