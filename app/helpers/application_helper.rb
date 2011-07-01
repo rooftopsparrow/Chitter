1be8904cb67c93318197f19683f078d762624280
Jonathan Nicholson <jonathandavidnicholson@gmail.com>
2011-07-01 10:59:55 -0600

    More tests and helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   
+  #Handle logo
+  def logo
+    image_tag("logo.png", :alt => "Chitter", :class => "round")
+  end
+  
   #Return a title on a per-page basis
   def title
     base_title = "Chitter"
