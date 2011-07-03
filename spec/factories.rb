9eebb3d4c524f0eb1fc602e51c5321dcb127e932
Jonathan Nicholson <jonathandavidnicholson@gmail.com>
2011-07-03 00:10:12 -0600

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Jonathan Nicholson"
+  user.email                  "jnicholson@example.com"
+  user.password               "password"
+  user.password_confirmation  "password"
+end
